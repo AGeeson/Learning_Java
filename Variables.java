20e262565d00288c20a817806f47a0fe76475193
AGeeson <alexanderwgeeson@gmail.com>
2020-10-10 11:37:05 +0100

    First commit

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args){
+        // double is the equivalant of a float in python
+        double networth = 10.00;
+        int age = 25;
+        String introduction = "My name is Alex, I'm learning java.";
+        boolean ismorethan = 3 > 2;
+        System.out.println(introduction + ", this is what i know so far... as well as how to compile and run java files" );
+    }
+}
